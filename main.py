ca1b6fc3bb9ded20a5b8250d4bb75f8c4dbb4cf6
Fati <2023171042@uteq.edu.mx>
2026-09-01 12:59:58 -0600

    Nuevos cambios en el backend2

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://back-movilidad-q9ckshrh6-fati2809s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
