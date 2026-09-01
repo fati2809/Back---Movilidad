cfff737b9cde4d6889a9faf1a4d30db473269655
Fati <2023171042@uteq.edu.mx>
2026-09-01 11:54:51 -0600

    Actualización del backend

--- main.py
+++ main.py
@@ -24,17 +24,16 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://*.vercel.app",
     ],
-
+    allow_origin_regex=r"https://.*\.vercel\.app|http://localhost:\d+|http://127\.0\.0\.1:\d+",
     allow_credentials=True,
-
     allow_methods=["*"],
-
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
