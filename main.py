5cb324210755071993fc0d2d2a9f23cdca0a56f3
Fati <2023171042@uteq.edu.mx>
2026-09-01 18:42:19 -0600

    fix: agregar nuevo dominio de netlify a CORS

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://back-movilidad-q9ckshrh6-fati2809s-projects.vercel.app",
+        "https://secretariademovilidad.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
